e7568559fa86dc52410a892b9befef32cdce87de
Jaehan's Github <jaehan127@gmail.com>
2017-01-27 12:42:51 +0900

    -
    -

--- Assets/Scripts/Monster/MonsterStateAppear.cs
+++ Assets/Scripts/Monster/MonsterStateAppear.cs
@@ -7,19 +7,16 @@ namespace SexyBackPlayScene
     {
         public MonsterStateAppear(Monster owner, MonsterStateMachine statemachine) : base(owner, statemachine)
         {
-
         }
 
         internal override void Begin()
         {
             owner.Animator.SetTrigger("Appear");
-            sexybacklog.Console("Appear Begin");
         }
 
         internal override void End()
         {
             owner.Animator.SetTrigger("Ready");
-            sexybacklog.Console("Appear End");
         }
 
         internal override void Update()
