04261b668f479c0bb252c309285c49822855e7cc
Jaehan's Github <jaehan127@gmail.com>
2017-03-27 22:42:48 +0900

    메뉴ui작성중.

--- Assets/MenuScene/Scripts/MenuScript.cs
+++ Assets/MenuScene/Scripts/MenuScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,8 +10,21 @@ namespace SexyBackMenuScene
     {
         private void Awake()
         {
-            //LoadGame();
+            InitUISetting();
         }
+
+        private void InitUISetting()
+        {
+            GameObject.Find("UI PopUp").transform.position = GameObject.Find("UI Root").transform.position;
+            GameObject.Find("UI PopUp").SetActive(false);
+            GameObject.Find("Middle_Window").SetActive(false);
+            GameObject.Find("Slot1New").SetActive(false);
+            GameObject.Find("Slot2New").SetActive(false);
+            GameObject.Find("SP").GetComponent<UILabel>().text ="";
+            GameObject.Find("GEM").GetComponent<UILabel>().text = "";
+            GameObject.Find("IconTable").transform.DestroyChildren();
+        }
+
         // Use this for initialization
         void Start()
         {
@@ -23,12 +37,12 @@ namespace SexyBackMenuScene
 
         }
 
-        public void NewGame()
+        public void TextNewGame()
         {
             SceneManager.LoadScene("PlayScene");
         }
 
-        public void LoadGame()
+        public void TestLoadGAme()
         {
             if (PlayerPrefs.HasKey("InstanceData"))
             {
@@ -44,14 +58,9 @@ namespace SexyBackMenuScene
             Application.Quit();
         }
 
-        private void OnDisable()
-        {
-            Debug.Log("디저블");
-        }
-
         private void OnDestroy()
         {
-            Debug.Log("디스트로이");
+            Debug.Log("메뉴신 소멸");
         }
     }
 
